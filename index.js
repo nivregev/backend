9719b45920e5de06a738716893cfde7cc1cd09d0
nivregev <nivregev2@gmail.com>
2023-10-03 17:59:25 +0300

    backend is ready

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from "express";
-import bcrypt from "bcrypt";
-import helmet from "helmet";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import multer from "multer";
+import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "module";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Posts.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,8 +46,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /*ROUTES WITH FILES */
-
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/post", verifyToken, upload.single("picture"), createPost);
+
+/*ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
@@ -52,6 +63,10 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Running on Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME*/
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
